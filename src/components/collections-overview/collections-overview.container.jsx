5ba38fa66dab1227444dc51d27f723909f21f48e
Weike Shi <shiw14@mcmaster.ca>
2020-08-22 23:23:51 -0400

    put asynchronous part to Redux, implemented Container pattern on CollectionPage and CollectionOverView

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,19 @@
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { compose } from "redux";
+
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selector";
+import WithSpinner from "../with-spinner/with-spinner.component";
+import CollectionsOverview from "./collections-overview.component";
+
+const mapStateToProps = createStructuredSelector({
+  //isLoading is used here to match the prop in withSpinner HOC
+  isLoading: selectIsCollectionFetching,
+});
+
+const CollectionsOverviewContainer = compose(
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
